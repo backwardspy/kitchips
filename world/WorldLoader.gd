cc2114de04ddf19916c1452a8686f084c32a6865
Chris Latham <backwardspy@gmail.com>
2021-04-04 12:28:26 +0100

    add file picker for loading chips

--- world/WorldLoader.gd
+++ world/WorldLoader.gd
@@ -4,21 +4,24 @@ signal craft_spawned
 
 const Craft = preload("res://chips/scripts/Craft.gd")
 
+var last_loaded_path: String
+
 func _ready():
-    load_craft()
+    load_craft("res://test/chips/basic.json")
     
 func _input(ev: InputEvent):
-    if ev is InputEventKey and ev.pressed and ev.scancode == KEY_U:
-        var craft := CraftController.get_active_craft()
-        if craft:
-            CraftController.set_active_craft(null)
-            craft.node.queue_free()
-            call_deferred("load_craft")
+    if ev is InputEventKey and ev.pressed and ev.scancode == KEY_U and last_loaded_path:
+        load_craft(last_loaded_path)
 
-func load_craft():
+func load_craft(path: String):
+    var active_craft := CraftController.get_active_craft()
+    if active_craft:
+        CraftController.set_active_craft(null)
+        active_craft.node.queue_free()
+    
     var loader := ChipLoader.new()
     var craft := loader.load_chips(
-        "res://test/chips/plane.json",
+        path,
         self,
         $SpawnPoint.global_transform.origin
     )
@@ -26,6 +29,8 @@ func load_craft():
         call_deferred("spawn_craft", craft)
     else:
         push_error("failed to load craft")
+    
+    last_loaded_path = path
 
 func spawn_craft(craft: Craft):
     get_tree().current_scene.add_child(craft.node)
