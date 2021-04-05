ba2d1db61d90ca9a90872eeed0e2eb9a09304637
Chris Latham <backwardspy@gmail.com>
2021-04-05 13:19:23 +0100

    add UI for loading chips and add initial lua support

---
+++ chips/basic.lua
@@ -0,0 +1,2 @@
+function update(delta)
+end
--- world/WorldLoader.gd
+++ world/WorldLoader.gd
@@ -7,7 +7,7 @@ const Craft = preload("res://chips/scripts/Craft.gd")
 var last_loaded_path: String
 
 func _ready():
-    load_craft("res://test/chips/basic.json")
+    load_craft("res://chips/basic.json")
     
 func _input(ev: InputEvent):
     if ev is InputEventKey and ev.pressed and ev.scancode == KEY_U and last_loaded_path:
@@ -28,7 +28,7 @@ func load_craft(path: String):
     if craft:
         call_deferred("spawn_craft", craft)
     else:
-        push_error("failed to load craft")
+        push_error("Failed to load craft")
     
     last_loaded_path = path
 
